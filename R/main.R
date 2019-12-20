101a76f2e4fa367cf3e60cef8e80e30696201783
Craig Jones <jones.dcraig@gmail.com>
2019-12-20 01:07:19 -0500

    update documentation

--- R/main.R
+++ R/main.R
@@ -9,6 +9,19 @@ JEA.Grey <- rgb(109/255, 110/255, 113/255, 0.5)
 #ff_info <- NULL
 
 
+#' Provides a framework for N^1.85 logarithmic graph based on NFPA 291 recommendations.
+#'
+#' @param MaxFlow Maximum Flow in GPM
+#'
+#' @return framework for drawing fireflow objects
+#' @export
+#'
+#' @examples
+#' n185()
+#'
+#' a <- ff(60,3000,20)
+#'
+#' draw(a)
 n185 <- function(MaxFlow=5000) {
   ff_info <<- NULL
 
@@ -41,6 +54,27 @@ n185 <- function(MaxFlow=5000) {
 
 }
 
+#' Draws Fireflow Objects
+#'
+#' @param fireflow fireflow object defined using ff function.
+#' @param color A specification for the default plotting color. See section ‘Color Specification’.
+#' @param LineType The line type. Line types can either be specified as an integer (0=blank, 1=solid (default), 2=dashed, 3=dotted, 4=dotdash, 5=longdash, 6=twodash)
+#'
+#' @seealso
+#' \code{\link{ff}}
+#' \code{\link{tilt}}
+#' \code{\link{shift}}
+#'
+#' @examples
+#' n185()
+#'
+#' a <- ff(60,3000,20, "first")
+#' b <- ff(50,2750, 35, "second")
+#'
+#' draw(a, "red", 1)
+#' draw(b, "blue", 2)
+#'
+#' draw_legend()
 draw <- function(fireflow, color="black", LineType=1) {
   tmp <- fireflow
   tmp$color <- color
@@ -59,7 +93,26 @@ draw <- function(fireflow, color="black", LineType=1) {
 
 }
 
-ff <- function(Ps, Qt, Pt, ID=""){#color="black", LineType = 1) {
+#' Creates a fire flow supply curve based on a single hydrant test per NFPA 291 Recommended Practice for Fire Flow Testing
+#'
+#' @param Ps Static Pressure from Field Fire Flow Test, in PSI
+#' @param Qt Test Flow from Field Fire Flow Test, in GPM
+#' @param Pt Test Residual from Field Fire Flow Test, in GPM
+#' @param ID Unique name for Field Fire Flow Test (used for legend)
+#'
+#' @return FireFlow Object
+#'
+#' @examples
+#' #' n185()
+#'
+#' a <- ff(60,3000,20, "first")
+#' b <- ff(50,2750, 35, "second")
+#'
+#' draw(a, "red", 1)
+#' draw(b, "blue", 2)
+#'
+#' draw_legend()
+ff <- function(Ps, Qt, Pt, ID="") {
   k_psi <- (Ps-Pt)/(Qt^1.85)
 
   ff <- structure(list(), class="fireflow")
@@ -74,13 +127,31 @@ ff <- function(Ps, Qt, Pt, ID=""){#color="black", LineType = 1) {
   return(ff)
 }
 
-shift <- function(fireflow, static, color="black", LineType=1) {
+#' Title
+#'
+#' @param fireflow
+#' @param static
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' n185()
+#'
+#' a <- ff(55, 4500, 50)
+#'
+#' draw(a)
+#'
+#' b <- shift(a, 40)
+#'
+#' draw(b, "grey50", 2)
+shift <- function(fireflow, static) {
   fireflow[1] <- static
 
   return(fireflow)
 }
 
-tilt <- function(fireflow, friction_factor,color="black", LineType=1) {
+tilt <- function(fireflow, friction_factor) {
   fireflow[2] <- unlist(fireflow[2])+friction_factor
 
   return(fireflow)
@@ -115,13 +186,13 @@ pt <- function(Qt=NULL, Pt=NULL, fireflow=NULL, color="black"){
   points(Qt^1.85,Pt, pch=21, bg="white", col=color, lwd=2, cex=1.5)
 }
 
-draw_legend(position="topright") {
+draw_legend <- function(position="topright") {
 
   legend(position
          , unname(unlist(ff_info[,"ID"]))
          , lwd=rep(2,nrow(ff_info))
          , lty=unname(unlist(ff_info[,"linetype"]))
-         , col=unname(unlist(ff_info[,"linetype"]))
+         , col=unname(unlist(ff_info[,"color"]))
          , inset=c(0.05,0.05)
          , seg.len = 4
          #, pch=PointSymbol
@@ -134,25 +205,3 @@ draw_legend(position="topright") {
          , text.font=2
   )
 }
-
-}
-
-Draw.Legend <- function(position="bottomleft",LineText, LineColor, LineType, PointSymbol=rep(NA,length(LineType)), LineWeight=rep(2,length(LineType))) {
-  legend(position
-         , LineText
-         , lwd=LineWeight
-         , lty=LineType
-         , col=LineColor
-         , inset=c(0.05,0.05)
-         , seg.len = 4
-         , pch=PointSymbol
-         , cex=0.8
-         , y.intersp=0.8
-         , bty = "n"
-         , box.col=rgb(1,1,1,0.75)
-         , bg=rgb(1,1,1,0.75)
-         , horiz=FALSE
-         , text.font=2
-  )
-}
-
