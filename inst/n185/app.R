46b10f371381cd65d59e8ddf3de6682bf0e86680
Craig Jones <jones.dcraig@gmail.com>
2019-12-26 15:13:23 -0500

    Updated UI in draw arbitrary number of fireflow tests

--- inst/n185/app.R
+++ inst/n185/app.R
@@ -26,8 +26,9 @@ ui <- fluidPage(theme=shinythemes::shinytheme("united"),
        , br()
        , div(style="display:inline-block;vertical-align:top;", fluidRow(
             column(5, actionButton("ff_draw", "Draw") ),
-            column(5, actionButton("ff_clear", "Clear") )
+            column(5, actionButton("ff_reset", "Reset") )
        )) #div
+       , div(textOutput("ff_msg"), style="color:red")
 
        , hr()
 
@@ -66,6 +67,58 @@ server <- function(input, output, session) {
   output$main_plot <- renderPlot( n185() )
 
   output$main_table <- renderTable( ff_info )
+
+  clear_fireflow <- function() {
+
+    updateNumericInput(session, "ff_static", value=0)
+    updateNumericInput(session, "ff_flow", value=0)
+    updateNumericInput(session, "ff_residual", value=0)
+    updateTextInput(session, "ff_id", value="")
+    updateColourInput(session, "ff_color", value="black")
+    updateSelectInput(session, "ff_linetype", selected=r_lty[1])
+
+  }
+
+  draw_prev_fireflow <- function(tmp_ff_info) {
+    if (!is.null(tmp_ff_info)) {
+      for (i in 1:nrow(tmp_ff_info)) {
+        draw(
+          ff(
+            as.numeric(tmp_ff_info[i,1])
+            , as.numeric(tmp_ff_info[i,3])
+            , as.numeric(tmp_ff_info[i,4])
+            , as.character(tmp_ff_info[i,5])
+          )
+          , as.character(tmp_ff_info[i,6])
+          , as.numeric(tmp_ff_info[i,7])
+        )
+      }
+    }
+  }
+
+  observeEvent( input$ff_draw, {
+
+    # Get parameters
+    tmp_ff <- ff( input$ff_static, input$ff_flow, input$ff_residual, input$ff_id )
+    tmp_col <- input$ff_color
+    tmp_lty <- match(input$ff_linetype, r_lty)
+
+    output$main_plot <- renderPlot({
+
+      tmp_ff_info <<- ff_info
+
+      n185()
+
+      draw_prev_fireflow(tmp_ff_info)
+
+      draw(tmp_ff, tmp_col, tmp_lty)
+
+
+    })
+
+    clear_fireflow()
+
+  }) # oberseveEvent ff_draw
 
 }
 
