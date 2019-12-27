672e79ef781523f07970a88ff66a616a0603e22e
Craig Jones <jones.dcraig@gmail.com>
2019-12-27 10:12:30 -0500

    moved helper functions to util.R

--- inst/n185/app.R
+++ inst/n185/app.R
@@ -3,6 +3,7 @@ library("shinythemes")
 library("colourpicker")
 
 source("def.R")
+source("util.R")
 
 ui <- fluidPage(theme=shinythemes::shinytheme("united"),
    sidebarLayout(
@@ -68,33 +69,7 @@ server <- function(input, output, session) {
 
   output$main_table <- renderTable( ff_info )
 
-  clear_fireflow <- function() {
 
-    updateNumericInput(session, "ff_static", value=0)
-    updateNumericInput(session, "ff_flow", value=0)
-    updateNumericInput(session, "ff_residual", value=0)
-    updateTextInput(session, "ff_id", value="")
-    updateColourInput(session, "ff_color", value="black")
-    updateSelectInput(session, "ff_linetype", selected=r_lty[1])
-
-  }
-
-  draw_prev_fireflow <- function(tmp_ff_info) {
-    if (!is.null(tmp_ff_info)) {
-      for (i in 1:nrow(tmp_ff_info)) {
-        draw(
-          ff(
-            as.numeric(tmp_ff_info[i,1])
-            , as.numeric(tmp_ff_info[i,3])
-            , as.numeric(tmp_ff_info[i,4])
-            , as.character(tmp_ff_info[i,5])
-          )
-          , as.character(tmp_ff_info[i,6])
-          , as.numeric(tmp_ff_info[i,7])
-        )
-      }
-    }
-  }
 
   observeEvent( input$ff_draw, {
 
