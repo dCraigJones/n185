4db4e07bfabef580788d8f951b77a7497804e99e
Craig Jones <jones.dcraig@gmail.com>
2019-12-27 16:21:30 -0500

    Changed the default color from Black to a Dark Blue

--- inst/n185/util.R
+++ inst/n185/util.R
@@ -4,7 +4,7 @@ clear_fireflow <- function(session) {
   updateNumericInput(session, "ff_flow", value=0)
   updateNumericInput(session, "ff_residual", value=0)
   updateTextInput(session, "ff_id", value="")
-  updateColourInput(session, "ff_color", value="black")
+  updateColourInput(session, "ff_color", value="#142B6C")
   updateSelectInput(session, "ff_linetype", selected=r_lty[1])
 
 }
